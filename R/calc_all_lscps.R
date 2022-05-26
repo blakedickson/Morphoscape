0c1ee5283ef8905d61d07d9c2699ac0e8d661b26
Noah Greifer <noah.greifer@gmail.com>
2022-05-26 15:16:41 -0400

    Improved speed and memory usage by calc_all_lscps()

--- R/calc_all_lscps.R
+++ R/calc_all_lscps.R
@@ -1,5 +1,5 @@
-calc_all_lscps <- function(kr_data, grid_weights, file = NULL, verbose = TRUE){
-
+calc_all_lscps <- function(kr_data, grid_weights, file = NULL){
+  
   if (!inherits(kr_data, "kriged_surfaces")){
     stop("'kr_data' must be a kriged_surfaces object, the output of a call to krige_surf().", call. = FALSE)
   }
@@ -10,26 +10,13 @@ calc_all_lscps <- function(kr_data, grid_weights, file = NULL, verbose = TRUE){
   
   func.names <- names(kr_data$autoKrige)
   
-  all_Wprime_surfs <- vector("list", nrow(grid_weights))
-  
-  if (verbose) {
-    cat("Calculating landscapes...\n")
-    pb <- txtProgressBar(min = 0, max = nrow(grid_weights), style = 3)
-  }
-  
-  for (i in 1:nrow(grid_weights)) {
-    all_Wprime_surfs[[i]] <- calc.W.kr(grid_weights[i,],
-                                       fnc_data = kr_data$dataframes,
-                                       func.names = func.names)
-    if (verbose) {
-      setTxtProgressBar(pb, i)
-    }
-  }
-  
-  if (verbose) close(pb)
-  
+  wtd_lscps <- lapply(kr_data$dataframes, function(x) {
+    as.matrix(x[func.names]) %*% t(grid_weights)
+  })
+
   all_lscps <- list(dataframes = kr_data$dataframes, 
-                    all_Wprime_surfs = all_Wprime_surfs)
+                    wtd_lscps = wtd_lscps,
+                    grid_weights = grid_weights)
   
   class(all_lscps) <- "all_lscps"
   
@@ -51,60 +38,12 @@ calc_all_lscps <- function(kr_data, grid_weights, file = NULL, verbose = TRUE){
   return(all_lscps)
 }
 
-calc_lscp <- function(kr_data, weights) {
-  if (!inherits(kr_data, "kriged_surfaces")){
-    stop("'kr_data' must be a kriged_surfaces object, the output of a call to krige_surf().", call. = FALSE)
-  }
-  
-  fn_dataframe <- kr_data$dataframes
-  func.names <- names(kr_data$autoKrige)
-  
-  if (!is.numeric(weights) || length(weights) != length(func.names)) {
-    stop("'weights' must be a vector of weights, one for each functional characteristic in 'kr_data'.", call. = FALSE)
-  }
-  names(weights) <- func.names
-  
-  out <- calc.W.kr(weights,
-                   fnc_data = fn_dataframe,
-                   func.names = func.names)
-  
-  #Output is class "wtd_lscp"
-  out
-}
-
-#Calculate Zprime values for a given vector of W
-#fnc_data is list containing grid and (optionally) new_data
-calc.W.kr <- function(W, fnc_data, func.names) {
-  
-  Wprime <- lapply(fnc_data, function(data) {
-    data[func.names] <- sweep(data[func.names], 2, W, FUN = "*")
-    Z <- rowSums(data[func.names])
-    
-    cbind(data, Z = Z)
-  })
-  
-  out <- list(W = W, Wprime = Wprime)
-  class(out) <- "wtd_lscp"
-  
-  out
-}
-
-print.wtd_lscp <- function(x, ...) {
-  cat("A wtd_lscp object\n")
-  cat("- weights:\n")
-  print(round(x[["W"]], 4))
-  if (!is.null(x[["Wprime"]][["new_data"]])) {
-    cat("- new data:\n\t", nrow(x[["Wprime"]][["new_data"]]), " rows\n\t",
-    "average Z = ", round(mean(x[["Wprime"]][["new_data"]][["Z"]]), 3), "\n", sep = "")
-  }
-}
-
 print.all_lscps <- function(x, ...) {
   cat("An all_lscps object\n")
   cat("- functional characteristics:\n\t", paste(names(x[["dataframes"]][["grid"]])[-(1:2)], collapse = ", "), "\n", sep = "")
-  cat("- number of landscapes:\n\t", length(x[["all_Wprime_surfs"]]), "\n", sep = "")
+  cat("- number of landscapes:\n\t", nrow(x[["grid_weights"]]), "\n", sep = "")
   cat("- weights incremented by:\n\t",
-      round(x[["all_Wprime_surfs"]][[1]][["W"]][1] - x[["all_Wprime_surfs"]][[2]][["W"]][1], 4),
+      round(x[["grid_weights"]][2,2], 4),
       "\n", sep = "")
   if (!is.null(x[["dataframes"]][["new_data"]])) {
     cat("- new data:\n\t", nrow(x[["dataframes"]][["new_data"]]), " rows\n", sep = "")
